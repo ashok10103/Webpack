979a646dd36726359819e7853029aefef1705592
ashok10103 <ashok10103@gmail.com>
2018-06-18 23:25:09 +0530

    added css to itemimage

--- src/components/itemimage/itemimage.js
+++ src/components/itemimage/itemimage.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import classes from './itemimage.css';
-
+import PizzaImage from '../../assets/pizza.jpg';
 
 const itemImage = (props)=>(
-    <div className={classes.ItemImage}></div>
+    <div className={classes.ItemImage}>
+        <img src = {PizzaImage} className = {classes.ItemImg} />
+    </div>
 )
