2563930ecabfc4d280a83cecabf4cb83f36e40fc
ashok10103 <ashok10103@gmail.com>
2018-06-24 12:07:28 +0530

    successfully completed production build

--- webpack.prod.conf.js
+++ webpack.prod.conf.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
-        chunkFilename: '[chunk]__[id].js',
+        chunkFilename: 'chunk__[id].js',
         publicPath: ''
     },
     resolve: {
